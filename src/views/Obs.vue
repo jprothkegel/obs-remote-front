9b97063522e0b92a396e7de6331c92ab8f3dae6d
jprothkegel <juan.rothkegel.12@sansano.usm.cl>
2019-07-28 16:09:19 -0400

    Added: Streaming status

--- src/views/Obs.vue
+++ src/views/Obs.vue
@@ -1,6 +1,6 @@
 <template>
     <v-layout align-center justify-center class="mt-4">
-        <v-flex xs12 sm8 md6>
+        <v-flex xs10 sm8 md6 lg4>
             <v-card class="elevation-10 mx-auto round-card">
                 <v-card-title class="justify-center">
                     Interactuar con OBS
@@ -46,12 +46,35 @@
                     </v-card>
 
                     <v-divider class="mt-2 mb-2"></v-divider>
+                    <v-layout row wrap justify-center align-center v-if="this.streaming != true">
+                        <v-flex xs12 class="text-xs-center">
+                            <v-btn outline round color="#48b400" v-if="this.getStatus" icon>
+                                <v-icon>play_arrow</v-icon>
+                            </v-btn>
+                            <v-btn disabled outline round color="#48b400" v-else icon>
+                                <v-icon>play_arrow</v-icon>
+                            </v-btn>
+                        </v-flex>
+                        <v-flex xs1></v-flex>
+                    </v-layout>
 
-                    <v-btn v-if="this.getStatus" @click="createVideo(streamKeyId,metadata,'Active', 'SR')" outline round block color="info">Transmitir & Grabar</v-btn>
-                    <v-btn v-else disabled outline round block color="info">Transmitir & Grabar</v-btn>
-
+                    <v-layout row wrap justify-center align-center v-else>
+                        <v-flex xs12 class="text-xs-center">
+                            <v-btn outline round color="error" icon>
+                                <v-icon>stop</v-icon>
+                                <div class="ringring2"></div>
+                            </v-btn>
+                        </v-flex>
+                    </v-layout>
+                    
+                    <v-btn v-if="this.getStatus" @click="createVideo(streamKeyId,metadata,'Active', 'SR')" outline block round color="info">Transmitir & Grabar </v-btn>
+                    
+                    <v-btn v-else disabled outline round block color="info">Transmitir & Grabar </v-btn><v-spacer></v-spacer>
                     <v-btn v-if="this.getStatus" @click="stopStreamingRecording" outline round block color="error">Detener</v-btn>
                     <v-btn v-else disabled outline round block color="error">Detener</v-btn>
+
+                    <v-btn @click="getStreamingStatus">get status</v-btn>
+                    <p> {{getStreamingStatus2}} </p>
 
                     <v-divider class="mt-2 mb-2"></v-divider>
                 </v-card-text>
@@ -72,10 +95,21 @@ export default {
                 language: '',
                 license: '',
                 creator: []
-            }
+            },
+            streaming: false
         }
     },
     methods: {
+        getStreamingStatus(){
+            this.$socket.sendObj({
+                'request-type': 'GetStreamingStatus',
+                'message-id': '10',
+            })
+            this.$options.sockets.onmessage = (data) => {
+                console.log("DATA2: ",JSON.parse(data.data))
+                this.streaming = JSON.parse(data.data).streaming
+            }
+        },
         connect(){
             this.$connect('ws://localhost:4444')
         },
@@ -146,6 +180,9 @@ export default {
     computed: {
         getStatus(){
             return this.$store.getters.obsStatus
+        },
+        getStreamingStatus2(){
+            return this.streaming
         }
     },
     mounted() {
@@ -164,5 +201,57 @@ export default {
 <style scoped>
 .round-card{
     border-radius: 10px;
+}
+
+.ring-container{
+    position: relative;
+    margin: auto;
+    left: 40%;
+    margin-bottom: 40%;
+}
+
+.circle {
+    width: 15px;
+    height: 15px;
+    background-color: #48b400;
+    border-radius: 50%;
+    position: absolute;
+    top: 5px;
+    left: 5px;
+    z-index: 500000000000000;
+}
+
+.ringring{
+    border: 3px solid #48b400;
+    -webkit-border-radius: 30px;
+    height: 25px;
+    width: 25px;
+    position: absolute;
+    /* left: 15px;
+    top: 15px; */
+    -webkit-animation: pulsate 1s ease-out;
+    -webkit-animation-iteration-count: infinite;
+    opacity: 0.0;
+    z-index: 50000000000;
+}
+
+.ringring2{
+    border: 3px solid #ff0000;
+    -webkit-border-radius: 30px;
+    height: 50px;
+    width: 50px;
+    position: absolute;
+    /* left: 15px;
+    top: 15px; */
+    -webkit-animation: pulsate 1s ease-out;
+    -webkit-animation-iteration-count: infinite;
+    opacity: 0.0;
+    z-index: 50000000000;
+}
+
+@-webkit-keyframes pulsate{
+    0% {-webkit-transform: scale(0.1, 0.1); opacity: 0.0;}
+    50% {opacity: 1.0;}
+    100% {-webkit-transform: scale(1.2,1.2); opacity: 0.0;}
 }
 </style>
